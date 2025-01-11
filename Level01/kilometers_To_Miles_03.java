635b07c537e983e44f4fd8f121d23a5d4992bc11
OjasJohar <ojasjohar@gmail.com>
2025-01-11 15:07:06 +0530

    kilometers To Miles file added

---
+++ Level01/kilometers_To_Miles_03.java
@@ -0,0 +1,18 @@
+//03. Create a program to convert the distance of 10.8 kilometers to miles.
+
+import java.lang.*;
+
+class kilometers_To_Miles_03{
+
+	public static void main(String[] args){
+                
+            // Given kilometers :
+	    double kilometers=10.8;
+		
+	    // Calculating miles from given kilometer :
+            double miles= kilometers * 1.6;
+		
+	    //Printing the miles we get after converting kilometer :
+            System.out.print("The distance "+ kilometers +" km in miles is "+ miles);
+	}
+}	
