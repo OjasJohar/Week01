8d552a5f61386ef173be8b54879dde8655e9a977
OjasJohar <ojasjohar@gmail.com>
2025-01-13 15:58:24 +0530

    Calculating Trigonometric functions

---
+++ Level01/CalculateTrigonometricFunctions_12.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CalculateTrigonometricFunctions_12 {
+
+    // Method to calculate trigonometric functions
+    public double[] calculateTrigonometricFunctions(double angle) {
+
+        // Convert angle from degrees to radians
+        double radians = Math.toRadians(angle);
+
+        // Calculate sine, cosine, and tangent
+        double sin = Math.sin(radians);
+        double cos = Math.cos(radians);
+        double tan = Math.tan(radians);
+
+        // Return the results in an array
+        double[] arr = { sin, cos, tan };
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        //Creating Scanner object
+		Scanner input = new Scanner(System.in);
+
+        CalculateTrigonometricFunctions_12 calculator = new CalculateTrigonometricFunctions_12();
+
+        //Taking input from the user in angle variable
+        System.out.println("Enter the angle(in degree):");
+        double angle = input.nextDouble();
+
+        //Calculate trigonometric functions
+        double[] results = calculator.calculateTrigonometricFunctions(angle);
+
+        //Display the results:
+        System.out.println("For angle " + angle + " degrees:");
+        System.out.println("Sine: " + results[0]);
+        System.out.println("Cosine: " + results[1]);
+        System.out.println("Tangent: " + results[2]);
+
+        //Closing Scanner class
+		input.close();
+    }
+}
